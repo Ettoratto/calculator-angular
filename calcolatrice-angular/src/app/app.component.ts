f09c0b22c76c4721219b4cc3134dba058fee490c
lai2-24 <lai2-24@itismeucci.com>
2024-04-03 06:42:55 +0000

    calcolatrice funzionante

--- calcolatrice-angular/src/app/app.component.ts
+++ calcolatrice-angular/src/app/app.component.ts
@@ -4,6 +4,7 @@ import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button'; 
 import {MatIconModule} from '@angular/material/icon';
 import {MatGridListModule} from '@angular/material/grid-list';
+import { evaluate } from 'mathjs';
 
 @Component({
   selector: 'app-root',
@@ -13,18 +14,47 @@ import {MatGridListModule} from '@angular/material/grid-list';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'calcolatrice-angular'
+  title = "Calcolatrice angular"
   calculation: string = ""
+  eraseAll: boolean = false
+  ans: string = ""
 
   function(type:string, value:any){
     if(type == "n"){
+      this.checkErase()
 
-
+      this.calculation += String(value)
     }else{
       switch (value) {
         case "DEL":
+          this.calculation = ""
+          break;
+
+        case "CANC":
+          this.calculation = this.calculation.slice(0, -1)
+          break;
+        
+        case "=":
+          if(eval(this.calculation) == undefined)
+            this.calculation = "MATH ERROR"
+          else{
+            this.calculation = eval(this.calculation)
+            this.ans = this.calculation
+            this.eraseAll = true
+          }
+          break;
+
+        case "ANS":
+          this.calculation += this.ans
           break;
       }
+    }
+  }
+
+  checkErase(){
+    if(this.eraseAll){
+      this.calculation = ""
+      this.eraseAll = false
     }
   }
 }
